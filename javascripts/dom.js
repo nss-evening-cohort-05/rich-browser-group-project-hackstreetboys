d3c42e03fe502ceefa61bb367691fa957d6b5a3d
Ben Greaves <bsgreaves@gmail.com>
2017-05-06 10:31:34 -0500

    Structure complete

---
+++ javascripts/dom.js
@@ -0,0 +1,11 @@
+var movieAPI = (function (oldDom) {
+
+	oldDom.writeDom = () => {
+		//prints cards to DOM. Takes an array of movies that the user has added
+		//Called in main.js
+	};
+
+
+
+	return oldDom;
+})(movieAPI || {});
---
+++ javascripts/user.js
@@ -0,0 +1,30 @@
+var movieAPI = (function (oldUser) {
+
+	olderUser.editMovie = () => {
+		//PROMISE that uses AJAX call to UPDATE movie in database
+		//Handles "watched" and "rating"
+		//use a PUT http call
+	};
+
+	olderUser.deleteMovie = () => {
+		//PROMISE that uses AJAX call to DELETE movie in database
+		//Handles "delete" button functionality
+		//Called in main.js
+		//use a DELETE http call
+
+	};
+
+	olderUser.addMovie = () => {
+		//PROMISE that uses AJAX call to ADD movie in database
+		//Handles "Save" button functionality
+		//Called in main.js
+		//use a POST http call
+	};
+
+	
+
+
+	
+
+	return oldUser;
+})(movieAPI || {});
