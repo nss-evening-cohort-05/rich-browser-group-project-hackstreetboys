509dd050730d7216c80978eafa10b01c7896f2c2
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-05-07 00:53:08 -0500

    add save movie

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	
 	let apiKeys = {};
+    let movie = Object();
 
-
+    
     let clearLogin = () => {
         $('#inputEmail').val("");
         $('#inputPassword').val("");
@@ -17,14 +18,38 @@ $(document).ready(function() {
         console.log("key errors", error);
     });
 
+    //Bao
     $('#getMovie').click((event) => {
         let movieTitle = $('#movieSearch').val();
         movieAPI.getMovie(movieTitle).then((results) => {
             console.log("Movie API results:", results);
+            if (results.Response == "False"){
+                 $("#saved-view").removeClass('hide');
+
+                $("#saved-view").html(`<div class="row col-xs-4 col-xs-offset-4" style="margin-top: 15px; color: red; font-size: 25px; font-weight: bolder;">${results.Error}</div>`);
+            }else {
+                movie.movieName = results.Title;
+                movie.rating = "";
+                movie.yearRelease = results.Released; 
+                movie.actors = results.Actors;
+                let movieString = `<div class="row col-xs-4 col-xs-offset-4" style="background-color: #fca27e ; border: 1px solid blue; margin-top: 15px;">`;
+                movieString += `<h3>${movie.movieName}</h3>`;
+                movieString += `<p>Released: ${movie.yearRelease}</p>`;
+                movieString += `<p>Actors: ${movie.actors}</p>`;
+                movieString += `<p>Rating: ${movie.rating}</p>`; 
+                movieString += `<button class="btn btn-primary col-xs-2 addMovie" id="addMovie">Save</button>`;
+                movieString += `</div>`;
+
+                $("#saved-view").html(movieString);
+                $("#saved-view").removeClass('hide');
+
+            };
+
         }).catch((error) => {
             console.log("getMovie Error", error);
         });
     });
+    //Bao
 
     //CLICK event to fire registerUser. Calls movieAPI.registerUser
     $('#registerButton').click((event) => {
@@ -85,6 +110,18 @@ $(document).ready(function() {
     
 
     //CLICK event to add movie to database.  Calls movieAPI.addMovie .then swap view and WriteDom
+    //Bao
+    $('.main-container').on('click', '.addMovie', (e) => {    
+        movieAPI.saveMovie(apiKeys, movie).then(() => {
+        $("#saved-view").append(`<div class="row col-xs-4 col-xs-offset-4" style="margin-top: 15px; color: red; font-size: 25px; font-weight: bolder;">Movie ${movie.movieName}  saved !</div>`);
+
+        $(e.target).remove();
+
+          }).catch((error) => {
+            console.log("saveMovie error", error);
+
+          }); 
+    });
 
     //CLICK event to delete movie from database.  Calls movieAPI.deleteMovie .then WriteDom
 
@@ -102,6 +139,10 @@ $(document).ready(function() {
     $("#saved-movies").click(() => {
         $("#search-view").addClass("hide");
         $("#saved-view").removeClass("hide");
+        
     });
+
+    
+
 
 });
